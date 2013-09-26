1fec54b747033539443c67ad10bf310f0907c273
Paul Odin <paul@aka.nu>
2013-09-25 20:21:18 -0700

    Remove attr_accessible from OpenID association model.

--- lib/association.rb
+++ lib/association.rb
@@ -3,7 +3,7 @@ require 'time'
 
 class Association < ActiveRecord::Base
   self.table_name = 'open_id_associations'
-  attr_accessible :server_url, :handle, :secret, :issued, :lifetime, :assoc_type
+  #attr_accessible :server_url, :handle, :secret, :issued, :lifetime, :assoc_type
   def from_record
     OpenID::Association.new(handle, secret, Time.at(issued), lifetime, assoc_type)
   end
